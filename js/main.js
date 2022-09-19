8bb383e238c428ce33b09be728eaeac610abd0f2
JO-DEV-IL <108283760+JO-DEV-IL@users.noreply.github.com>
2022-09-19 10:49:26 -0400

    Add files via upload
    Uploading project in it's current state

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// STICKY NAVBAR //
+window.onscroll = function() {stickyNav()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}

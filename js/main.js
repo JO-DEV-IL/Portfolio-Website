0d2ae83e96f22ff20744e30fe524cdd8bd1d37c9
Jillian O'Keefe <jillianokeefe.dev@gmail.com>
2022-09-19 11:58:22 -0400

    Updated css and js

--- js/main.js
+++ js/main.js
@@ -9,22 +9,3 @@ function stickyNav() {
     navbar.classList.remove("sticky");
   }
 }
-
-const i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    const elem = document.getElementById("myBar");
-    const width = 1;
-    const id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-}

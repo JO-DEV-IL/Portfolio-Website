443a06d35e0fa423d663dc90adbee779dce0bac8
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-09-30 15:41:19 -0400

    updates to style

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // STICKY NAVBAR //
-window.onscroll = function() {stickyNav()};
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
-function stickyNav() {
-  if (window.pageYOffset >= sticky) {
+window.onscroll = function() {stickyNav()}
+
+const navbar = document.getElementById("navbar")
+const sticky = navbar.offsetTop
+
+function stickyNav(){
+  if (window.pageYOffset >= sticky){
     navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky")
   }
 }
 
